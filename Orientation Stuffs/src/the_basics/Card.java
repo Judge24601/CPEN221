bd4f8d2e5ab70943cf269e459c93de044680d8c1
Judge24601 <braedenjury@gmail.com>
2017-09-10 11:08:04 -0700

    Working on a fun thing

--- Orientation Stuffs/src/the_basics/Card.java
+++ Orientation Stuffs/src/the_basics/Card.java
@@ -1,5 +1,11 @@
 package the_basics;
 
 public class Card {
-
+	private int value, suit;
+	public Boolean shown;
+	public Card(int value, int suit) {
+		this.value = value;
+		this.suit = suit;
+		shown = false;
+	}
 }
